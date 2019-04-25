1aa9070a6ee28dda026e2d24701ac70df0689d07
RamKhalsa <khalsa.ramdas@gmail.com>
2019-04-25 10:44:55 -0700

    Implement seed script for PostgreSQL and Cassandra

--- database/seed/schema.sql
+++ database/seed/schema.sql
@@ -19,11 +19,21 @@ CREATE TABLE IF NOT EXISTS stock_info (
 );
 
 CREATE TABLE IF NOT EXISTS stock_quotes (
- stock_id INTEGER REFERENCES stock_info(id),
- price NUMERIC (6, 2),
+ id SERIAL PRIMARY KEY,
+ stock_id INTEGER,
+ price NUMERIC (7, 2),
  time_stamp TIMESTAMPTZ,
  label VARCHAR (30)
 );
 
 \copy stock_info FROM PROGRAM 'gunzip -c $(pwd)/database/dataGeneration/stockData.csv.gz' WITH DELIMITER '|' CSV HEADER;
-\copy stock_quotes FROM PROGRAM 'gunzip -c $(pwd)/database/dataGeneration/quoteData.csv.gz' WITH DELIMITER '|' CSV HEADER;
+\copy stock_quotes(stock_id, price, time_stamp, label) FROM PROGRAM 'gunzip -c $(pwd)/database/dataGeneration/quoteData.csv.gz' WITH DELIMITER '|' CSV HEADER;
+
+ALTER TABLE stock_quotes
+ADD CONSTRAINT constraint_fk  FOREIGN KEY (stock_id) REFERENCES stock_info (id);
+
+CREATE INDEX ticker_idx ON stock_info (ticker);
+CREATE INDEX stock_id_idx ON stock_quotes (stock_id);
+CREATE INDEX label_idx ON stock_quotes (label);
+
+ANALYZE;
