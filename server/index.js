f5f739b5945fc4fae6354eaa71ec891c491609ed
RamKhalsa <khalsa.ramdas@gmail.com>
2019-04-30 21:10:30 -0700

    implement Loader.io route

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ const port = PORT || 8080;
 app.use(cors());
 mountRoutes(app);
 
+app.get('/loaderio-4b30aa4e4ad8fb836f7a36b6947eec42', (req, res) => {
+  res.send('loaderio-4b30aa4e4ad8fb836f7a36b6947eec42');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/stocks/:stockId', express.static(join(__dirname, '/../public/dist')));
